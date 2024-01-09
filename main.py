166b67ea3ac257003cde02c13cc60531db84f782
wilfex <wilfex81@gmail.com>
2024-01-09 21:40:47 +0300

    commands and events:

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from dotenv import load_dotenv
 import os
-import token
+import requests
+import json
 import discord
 from discord.ext import commands
 
 load_dotenv()
 
 intents = discord.Intents.all()
+
 client = commands.Bot(command_prefix='!', intents=intents)
 
 
@@ -24,6 +26,30 @@ async def hello(ctx):
 @client.command()
 async def goodbye(ctx):
     await ctx.send("so sad to see you go😭😭")
+
+
+@client.event
+async def on_member_join(member):
+
+    JOKEAPI = os.getenv('JOKEAPIKEY')
+    dad_jokes = "https://dad-jokes.p.rapidapi.com/random/joke"
+
+    headers = {
+        "X-RapidAPI-Key": JOKEAPI,
+        "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
+    }
+
+    response = requests.get(dad_jokes, headers=headers)
+
+    channel = client.get_channel(1194302076952531048)
+    await channel.send("Welcome to the server!, How about a nice dad joke!! UUH?")
+    await channel.send(json.load(response.text)['content'])
+
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(1194302076952531048)
+    await channel.send("Goodbye!")
 
 TOKEN = os.getenv('BOT_AUTH')
 client.run(TOKEN)
